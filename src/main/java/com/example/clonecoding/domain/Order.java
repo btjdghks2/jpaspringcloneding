a9ad1f8ed10e64382ea936e64af370c42a0e645c
btjdghks2 <btjdghks2@gmail.com>
2023-07-06 12:13:25 +0900

    jpa refactoring

--- src/main/java/com/example/clonecoding/domain/Order.java
+++ src/main/java/com/example/clonecoding/domain/Order.java
@@ -16,18 +16,38 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order_id")
+    @OneToMany(mappedBy = "order_id", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY , cascade = CascadeType.ALL)
     @JoinColumn(name = "deliery_id")  //연관관계의 주인은 오더,
     private Delivery delivery;
 
     private LocalDateTime orderDate; //주문시간
 
     private OrderStatus status; // 주문상태
+
+
+    // 연관관계 메서드, 들고 있는 쪽이(연관관꼐 주인이)
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+
 }
