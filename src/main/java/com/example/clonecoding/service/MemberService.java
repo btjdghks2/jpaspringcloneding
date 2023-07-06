e0eaff276d4c79c34b8d70873075d50a1dbb1cad
btjdghks2 <btjdghks2@gmail.com>
2023-07-06 17:19:06 +0900

    service logic

---
+++ src/main/java/com/example/clonecoding/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.example.clonecoding.service;
+
+import com.example.clonecoding.domain.Member;
+import com.example.clonecoding.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+
+        List<Member> findMembers =  memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("중복 회원입니다.");
+        }
+    }
+
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
