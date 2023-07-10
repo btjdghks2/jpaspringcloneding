1ff08ee90274a7fb6804d6babf46aedeaf874db5
btjdghks2 <btjdghks2@gmail.com>
2023-07-10 17:03:38 +0900

    relax

---
+++ src/main/java/com/example/clonecoding/controller/BookForm.java
@@ -0,0 +1,17 @@
+package com.example.clonecoding.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookForm {
+
+    private String Id;
+
+    private String Name;
+
+    private String Price;
+    private String StockQuantity;
+
+}
---
+++ src/main/java/com/example/clonecoding/controller/ItemController.java
@@ -0,0 +1,32 @@
+package com.example.clonecoding.controller;
+
+
+import com.example.clonecoding.domain.item.Book;
+import com.example.clonecoding.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item);
+
+        return "";
+    }
+
+}
