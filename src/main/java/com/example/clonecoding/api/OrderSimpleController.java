c4681514b79723271ef0a3a036b7b020659379f4
btjdghks2 <btjdghks2@gmail.com>
2023-07-13 18:33:49 +0900

    v3 update

--- src/main/java/com/example/clonecoding/api/OrderSimpleController.java
+++ src/main/java/com/example/clonecoding/api/OrderSimpleController.java
@@ -1,6 +1,8 @@
 package com.example.clonecoding.api;
 
+import com.example.clonecoding.domain.Address;
 import com.example.clonecoding.domain.Order;
+import com.example.clonecoding.domain.OrderStatus;
 import com.example.clonecoding.repository.OrderRepository;
 import com.example.clonecoding.repository.OrderSearch;
 import com.example.clonecoding.repository.SimpleOrderQueryDto;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,16 +29,32 @@ public class OrderSimpleController {
     }
 
     @GetMapping("api/v3/simple-orders")
-    public List<SimpleOrderDto> irdersV3() {
+    public List<SimpleOrderDto> ordersV3() {
         List<Order> order = orderRepository.findAllWithMemberDelivery();
         List<SimpleOrderDto> result = order.stream()
                 .map(o -> new SimpleOrderDto(o))
                 .collect(Collectors.toList());
+
+        return result;
     }
 
     @Data
     static class SimpleOrderDto {
+
         private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+
 
     }
 
