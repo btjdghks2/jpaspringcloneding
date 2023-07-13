f28db5f3af8214dcfa0a019fd7ec00301406f349
btjdghks2 <btjdghks2@gmail.com>
2023-07-13 18:22:16 +0900

    jpa dto detail

--- src/main/java/com/example/clonecoding/api/OrderSimpleController.java
+++ src/main/java/com/example/clonecoding/api/OrderSimpleController.java
@@ -3,11 +3,14 @@ package com.example.clonecoding.api;
 import com.example.clonecoding.domain.Order;
 import com.example.clonecoding.repository.OrderRepository;
 import com.example.clonecoding.repository.OrderSearch;
+import com.example.clonecoding.repository.SimpleOrderQueryDto;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,4 +24,28 @@ public class OrderSimpleController {
 
         return all;
     }
+
+    @GetMapping("api/v3/simple-orders")
+    public List<SimpleOrderDto> irdersV3() {
+        List<Order> order = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = order.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<SimpleOrderQueryDto> orderV4() {
+        return orderRepository.findOrderDtos();
+
+    }
+
+
+
+
 }
--- src/main/java/com/example/clonecoding/repository/OrderRepository.java
+++ src/main/java/com/example/clonecoding/repository/OrderRepository.java
@@ -37,5 +37,26 @@ public class OrderRepository {
     }
 
     public List<Order> findAllByString(OrderSearch orderSearch) {
+        return null; //쓰다만거
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        "join fetch o.member m " +
+                        "join fetch o.delivery d" , Order.class)
+                .getResultList();
+    }
+
+    public List<SimpleOrderQueryDto> findOrderDtos() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
+                        "from Order o" +
+                        "join o.member m" +
+                        "join o.delivery d", SimpleOrderQueryDto.class)  //dto를 쿼리에 직접 적으르려면 select 옆에 new 를 선언해주고 dto의 주소를 적어줘야 한다
+                .getResultList();
+
+        // 여기 로직은 jpql로 일일히 설정해 쿼리를 최적화 한것, v3의 방법보다 최적화가 잘 되지만 직접 적었기 때문에 유연성이 떨어진다
+
     }
 }
